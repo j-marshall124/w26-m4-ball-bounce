967f7f18e515f7e080eb605550e6561065271c7e
j-marshall124 <jordan.marshall1@mohawkcollege.ca>
2026-02-26 16:36:59 -0500

    Add bounce and keep on screen

--- ball-bounce/Game.cs
+++ ball-bounce/Game.cs
@@ -10,11 +10,13 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        // Place your variables here:
+        // Ball
         Vector2 position = new Vector2(200, 50);
         Vector2 velocity;
         float radius = 25;
+        // Physics
         Vector2 gravity = new Vector2(0, 8);
+        float forceKept = 0.50f; // 50%
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -34,6 +36,15 @@ namespace MohawkGame2D
             // Move ball
             velocity += gravity;
             position += velocity * Time.DeltaTime;
+
+            //Check if we are below the screen height / bottom edge
+            if (position.Y + radius > Window.Height)
+            {
+                // Move ball up to bottom edge
+                position.Y = Window.Height - radius;
+                // Invert velocity to bounce up, scale velocity down a bit
+                velocity.Y = -velocity.Y * forceKept;
+            }
 
             // Draw ball
             Draw.LineSize = 1;
